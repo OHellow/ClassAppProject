999d2d048b42558562fe72dbe9e56d6b2398480c
Your NameOHellow <olegsatol@gmail.com>
2020-02-17 00:36:13 +0300

    first commit

---
+++ ClassAppDev/ClassAppDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassAppDev
+//
+//  Created by Satsishur on 17.02.2020.
+//  Copyright © 2020 swiftlesson2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
